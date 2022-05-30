ad03af8008acdca9572a1c2bf55a26b1953a3d9d
Brenda309 <brewihogora23@gmail.com>
2022-05-30 23:09:52 +0200

    Add date to display local time

--- modules/date.js
+++ modules/date.js
@@ -1,7 +1,14 @@
 import { DateTime } from '../luxon.js';
+console.log("hi")
 class date{
     static dates (){
     const time = DateTime.now().toLocaleString({
+        month: 'long',
+        day: 'numeric',
+        year: 'numeric',
+        hour: 'numeric',
+        minute: '2-digit',
+        second: '2-digit',
     })
       const getDate=  document.querySelector('.date');
       getDate.innerHTML = time;
